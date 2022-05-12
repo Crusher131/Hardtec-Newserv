ce2284168c2ab8e5359c1fb6431351130991c177
Crusher131 <35925546+Crusher131@users.noreply.github.com>
2022-05-12 15:38:41 -0300

    Update screenshow.sh

--- screenshow.sh
+++ screenshow.sh
@@ -1,49 +1,43 @@
+#!/bin/bash
 
-
-#!/bin/sh
-#
-[ -r /etc/lsb-release ] && . /etc/lsb-release
-
-if [ -z "$DISTRIB_DESCRIPTION" ] && [ -x /usr/bin/lsb_release ]; then
-        # Fall back to using the very slow lsb_release utility
-        DISTRIB_DESCRIPTION=$(lsb_release -s -d)
-fi
-
-screenfetch
-
-echo -e "\033[38;2;0;0;255m"
 echo "Servidor Instalado e configurado por"
-echo -e " __  __     ______     ______     _____     ______   ______     ______    
-/\ \_\ \   /\  __ \   /\  == \   /\  __-.  /\__  _\ /\  ___\   /\  ___\   
-\ \  __ \  \ \  __ \  \ \  __<   \ \ \/\ \ \/_/\ \/ \ \  __\   \ \ \____  
- \ \_\ \_\  \ \_\ \_\  \ \_\ \_\  \ \____-    \ \_\  \ \_____\  \ \_____\ 
-  \/_/\/_/   \/_/\/_/   \/_/ /_/   \/____/     \/_/   \/_____/   \/_____/ "
-  echo -e "Hardtec.srv.br"
-  cat /etc/redhat-release
+echo -e "\033[38;2;0;0;255m"
+echo -e "HH   HH   AAA   RRRRRR  DDDDD   TTTTTTT EEEEEEE  CCCCC  
+HH   HH  AAAAA  RR   RR DD  DD    TTT   EE      CC    C 
+HHHHHHH AA   AA RRRRRR  DD   DD   TTT   EEEEE   CC      
+HH   HH AAAAAAA RR  RR  DD   DD   TTT   EE      CC    C 
+HH   HH AA   AA RR   RR DDDDDD    TTT   EEEEEEE  CCCCC  "
 
 echo -e "\033[m"
+  echo -e "Hardtec.srv.br 47 3521-2335"
+  cat /etc/redhat-release
 
 printf "\n"
-
+screenfetch
 #Interfaces
 INTERFACE=$(ip -4 ad | grep 'state UP' | awk -F ":" '!/^[0-9]*: ?lo/ {print $2}')
 
-printf "\n"
-printf "Interface\tMAC Address\t\tIP Address\t\n"
-
 for x in $INTERFACE
 do
-        MAC=$(ip ad show dev $x |grep link/ether |awk '{print $2}')
         IP=$(ip ad show dev $x |grep -v inet6 | grep inet|awk '{print $2}')
-        printf  $x"\t\t"$MAC"\t"$IP"\t\n"
+        echo -e " \033[0m\033[1;33mIP: \033[m$IP"
 
 done
-echo
-UPDATES_COUNT=$(yum check-update --quiet | grep -v "^$" | wc -l)
+GATEWAY=$(route -n|grep '^0.0.0.0'|awk '{print $2}')
+        echo -e " \033[0m\033[1;33mGATEWAY: \033[m$GATEWAY"
 
-if [[ $UPDATES_COUNT -gt 0 ]]; then
-  echo "Updates available: ${UPDATES_COUNT}"
-else
-  echo "No packages marked for update"
-fi
-echo
+DNS=$(cat /etc/resolv.conf|grep '^nameserver'|grep -v ':'|awk '{print $2}')
+DNSNUMBER=1
+for x in $DNS
+do
+        echo -e "\033[0m\033[1;33m DNS$DNSNUMBER: \033[m$x"
+((DNSNUMBER=DNSNUMBER+1))
+done
+
+
+
+
+    echo -e "\033[0m\033[1;33m DISK: \033[m\n"
+    df -lh |egrep '/dev/sd|/dev/mapper' |awk '{print $1,"\t" $6,"\t\t" $3,"/ " $4, "("$5")"}'
+
+exit 0
