860849acd8e3fcb36a4cae88cf2a94f959210daa
Crusher131 <35925546+Crusher131@users.noreply.github.com>
2022-05-13 09:07:38 -0300

    Update Initinstall.sh

--- Initinstall.sh
+++ Initinstall.sh
@@ -14,7 +14,7 @@ echo -e " __  __     ______     ______     _____     ______   ______     ______
 echo -e "\033[m"
 
 BASIC_PACKAGES(){
-    CHECKPACKAGES=(epel-release vim-enhanced yum-utils iotop rsync htop screen screen curl wget rsync ntsysv samba acpid)
+    CHECKPACKAGES=(epel-release vim-enhanced yum-utils iotop rsync htop screen screen curl wget rsync ntsysv samba acpid bc)
     INSTALLPACKAGES=()
     INSTALLEDPACKAGES=()
 echo "Limpando Cache"
