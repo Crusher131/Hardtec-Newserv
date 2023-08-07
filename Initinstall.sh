6f3028c0e2cb893bc5d962eb4f20c41fa411b252
Jeferson Zacarias Sens <35925546+Crusher131@users.noreply.github.com>
2023-08-07 14:30:34 -0300

    Update Initinstall.sh

--- Initinstall.sh
+++ Initinstall.sh
@@ -64,7 +64,7 @@ yum upgrade -y >> /dev/null
 
 BASIC_PACKAGES(){
     CHECKPACKAGES=(epel-release vim-enhanced yum-utils iotop rsync htop screen screen curl wget rsync ntsysv samba acpid bc net-tools xinetd mtools
-mlocate)
+mlocate ncdu)
     INSTALLPACKAGES=()
     INSTALLEDPACKAGES=()
 echo "Limpando Cache"
